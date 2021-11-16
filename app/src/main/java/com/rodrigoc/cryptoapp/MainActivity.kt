66d225bb9abc0a83f73262c684774066ea6e26a9
Rodrigo de Oliveira Cavalcante <rodrigoliveiracavalcante@gmail.com>
2021-11-16 11:44:11 -0300

    DetailScreen

--- app/src/main/java/com/rodrigoc/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/rodrigoc/cryptoapp/MainActivity.kt
@@ -8,14 +8,40 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.Navigation
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.rodrigoc.cryptoapp.presentation.Screen
+import com.rodrigoc.cryptoapp.presentation.coin_list.components.CoinDetailScreen
+import com.rodrigoc.cryptoapp.presentation.coin_list.components.CoinListScreen
 import com.rodrigoc.cryptoapp.presentation.ui.theme.CryptoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoAppTheme {
-
+                Surface(color = MaterialTheme.colors.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
+                }
             }
         }
     }
